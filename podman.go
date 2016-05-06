76f1f56bb380f315e7c5e1a3e405e512daa1696f
Hunter Werlla <Hunter@werlla.com>
2016-05-05 22:27:57 -0500

    changed to store defaults in program, started to work on JSON read write of settings

--- podman.go
+++ podman.go
@@ -1,28 +1,47 @@
 package main
 
 import (
-	//"encoding/json" //for decoding search data from itunes
+	"encoding/json" //for decoding search data from itunes
 	"flag"
 	"fmt"
+	"os"
 	"strings"
 	//"github.com/krig/go-sox" //for playing podcasts
 )
 
+type Configuration struct {
+	storageLocation string
+	upKeybind       string
+	downKeybind     string
+	leftKeybind     string
+	rightKeybind    string
+	playKeybind     string
+	searchKeybind   string
+	subscribed      []PodcastEntry
+}
+
 type PodcastEntry struct {
+	name   string
 	author string
+	uri    string
 }
 
 func main() {
+	//make configurationg struct that holds default settings
+	config := Configuration{"~/podman", "<up>", "<down>", "<left>", "<right>", "<space>", "/", make([]PodcastEntry, 0)}
 	//read command line flags first
-	guiSelection := flag.Bool("no-tui", false, "Select whether to use the TUI or not")
+	noTui := flag.Bool("no-tui", false, "Select whether to use the TUI or not")
 	flag.Parse()
+	//read config file
+	config = readConfig(config)
 	//made a decision to use TUI or not
-	if *guiSelection == true {
+	if *noTui == true {
 		end := false
 		for end != true {
 			end = CliInterface()
 		}
 	} else {
+		//TUI
 		panic("unimplemented")
 	}
 }
@@ -53,5 +72,40 @@ func CliInterface() bool {
 }
 
 func getSubscribed() []PodcastEntry {
-	return make([]PodcastEntry, 5)
+	return make([]PodcastEntry, 1)
+}
+
+//read config in
+func readConfig(c Configuration) Configuration {
+	//check if there is a config file
+	config, err := os.Open("./config.json")
+	if err != nil {
+		//config does not exist so build one out of the defult settings
+		//first check if the storage location is ok
+		if _, err := os.Stat(c.storageLocation); os.IsNotExist(err) {
+			//path does not exist try to make
+			err := os.Mkdir(c.storageLocation, 0666)
+			if err != nil {
+				//failed to create folder to store, store files in same directory as program
+				c.storageLocation = "."
+			}
+		}
+		writeConfig(c)
+		return c
+	}
+	defer config.Close()
+	//now read in the settings and write it to the configuration object
+	return c
+}
+
+//save current config to file
+func writeConfig(c Configuration) {
+	config, err := os.Create("./config.json")
+	if err != nil {
+		//using default settings because cannot write settings
+		return
+	}
+	defer config.Close()
+	encoder := json.NewEncoder(config)
+	encoder.Encode(c)
 }
