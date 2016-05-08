6a3836396f4ac1d573470481af312b0da1bc06c5
Hunter Werlla <Hunter@werlla.com>
2016-05-08 17:33:54 -0500

    can switch which file is playing now

--- play.go
+++ play.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/krig/go-sox"
 )
 
 //this runs on its own thread to start/stop and select the media that is playing, it will also skip ahead in the future
 //TODO make it skip ahead
 func play(fileNameChannel chan string, control chan int) {
+	var (
+		chain   *sox.EffectsChain = nil
+		inFile  *sox.Format       = nil
+		outFile *sox.Format       = nil
+	)
+	if !sox.Init() {
+		panic("Unable to start the player")
+	}
+	defer sox.Quit()
 	for {
 		//wait for a signal
 		status := -1
@@ -17,25 +27,32 @@ func play(fileNameChannel chan string, control chan int) {
 		}
 		//if filname is not empty, then new filename recieved
 		if fileName != "" {
-			if !sox.Init() {
-				panic("Unable to start the player")
+			//when switching stop before playing new file
+			if chain != nil {
+				chain.DeleteAll()
+				chain.Release()
 			}
-			defer sox.Quit()
+			if inFile != nil {
+				inFile.Release()
+			}
+			if outFile != nil {
+				outFile.Release()
+			}
+			//stop sox if it's running
+			//now start playing
+			fmt.Println("playing")
 			//first time get the fileName
-			inFile := sox.OpenRead(fileName)
-			defer inFile.Release()
+			inFile = sox.OpenRead(fileName)
 			//TODO make it work on windows too
-			out := sox.OpenWrite("default", inFile.Signal(), nil, "alsa")
-			if out == nil {
-				out = sox.OpenWrite("default", inFile.Signal(), nil, "pulseaudio")
-				if out == nil {
+			outFile = sox.OpenWrite("default", inFile.Signal(), nil, "alsa")
+			if outFile == nil {
+				outFile = sox.OpenWrite("default", inFile.Signal(), nil, "pulseaudio")
+				if outFile == nil {
 					panic("Cannot open audio output devices")
 				}
 			}
-			defer out.Release()
 			//Now actually play
-			chain := sox.CreateEffectsChain(inFile.Encoding(), out.Encoding())
-			defer chain.Release()
+			chain = sox.CreateEffectsChain(inFile.Encoding(), outFile.Encoding())
 			//make it output
 			interm_signal := inFile.Signal().Copy()
 			//set input
@@ -45,16 +62,25 @@ func play(fileNameChannel chan string, control chan int) {
 			e.Release()
 			//set output
 			e = sox.CreateEffect(sox.FindEffect("output"))
-			e.Options(out)
+			e.Options(outFile)
 			chain.Add(e, interm_signal, inFile.Signal())
 			e.Release()
 			//process which also plays
-			chain.Flow()
+			go chain.Flow()
 		} else {
 			switch status {
 			case 1:
 				return
 			}
 		}
+	}
+	if inFile != nil {
+		inFile.Release()
+	}
+	if outFile != nil {
+		outFile.Release()
+	}
+	if chain != nil {
+		chain.Release()
 	}
 }
